7670b91edb22c0c75f0dacc2d9ca413f4c5d9748
naina <NAINA>
2024-08-23 16:37:37 +0530

    implemented ui

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,6 +1,8 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+  const anchorContainer = document.createElement('div');
+  anchorContainer.className = 'anchor-container'; // Add a class for styling if needed
 
   // setup image columns
   [...block.children].forEach((row) => {
@@ -12,6 +14,21 @@ export default function decorate(block) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
         }
+      }
+      const paragraphs = col.querySelectorAll('div > p');
+      if (paragraphs.length > 0) {
+        paragraphs.forEach((para, index) => {
+          para.classList.add(`tk-${index + 1}`);
+          const anchor = para.querySelector('a');
+          if (anchor) {
+            anchorContainer.appendChild(para);
+          }
+        });
+      }
+      const tk2Paragraph = block.querySelector('.tk-2');
+
+      if (tk2Paragraph) {
+        tk2Paragraph.insertAdjacentElement('afterend', anchorContainer);
       }
     });
   });
