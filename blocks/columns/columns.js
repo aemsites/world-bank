1dba664b126cd3dcdabc814a7ad0c48cbf3c5b4b
Thomas AS <thomas.jsm13@gmail.com>
2024-08-24 14:25:48 +0530

    adding some checks

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -27,9 +27,9 @@ export default function decorate(block) {
     cards.forEach((card) => {
       // eslint-disable-next-line no-unused-vars
       const [_title, thumbnail, description, ...button] = [...card.children];
-      const picture = thumbnail.querySelector('picture');
-      thumbnail.replaceWith(picture);
-      description.classList.add('columns-feature-card-description');
+      const picture = thumbnail?.querySelector('picture');
+      thumbnail?.replaceWith(picture);
+      description?.classList.add('columns-feature-card-description');
 
       const cardContent = div({ class: 'columns-feature-card-content' }, description, ...button);
       const cardGroup = div({ class: 'columns-feature-card-group' }, picture, cardContent);
