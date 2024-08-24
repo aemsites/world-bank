00ea7f9c8f281e613849d48acfc7043779fc7bf1
Arjun Singh - UG <Arjun.3.Singh@coforge.com>
2024-08-24 15:18:10 +0530

    styling updates

--- blocks/explore-cards/explore-cards.js
+++ blocks/explore-cards/explore-cards.js
@@ -9,48 +9,40 @@ export default function decorate(block) {
     // Create and style the main container
     const developmentStories = div({ class: 'development-stories' });
 
-    // const developmentStories = document.createElement('div');
-    // developmentStories.classList.add('development-stories');
-
     // Style and append the heading to the main container
     const heading = block.querySelector('.explore-cards > div:first-child');
     heading.classList.add('explore-title');
     developmentStories.appendChild(heading);
 
     // Create a container for all the cards
-    // const storiesContainer = document.createElement('div');
     const storiesContainer = div({ class: 'stories-container' });
-    // storiesContainer.classList.add('stories-container');
 
     // Iterate through each card and transform it
     cards.forEach(card => {
-        //  const storyCard = document.createElement('div');
-        //  storyCard.classList.add('story-card');
-
         // Extract elements from the card
         const [imageContainer, altText, contentType, storyType, storyHeading, tags, link] = card.children;
         const storyCard = div({ class: 'story-card' }
         );
 
-        var iconsrc;
-        const contentTypeText = contentType.textContent;
-        if (contentTypeText == 'podcast') {
-            iconsrc = '/icons/podcast.svg';
-        } else if (contentTypeText == 'video') {
-            iconsrc = '/icons/playbutton.svg'
-        }
-        const contentTypeDiv = div({ class: 'contenttype-icon' }, img({src: iconsrc}));
-
-        storyCard.appendChild(contentTypeDiv);
-        // storyCard.classList.add(contentType.textContent);
         imageContainer.getElementsByTagName('img')[0].setAttribute("alt", altText.textContent);
 
         // Style the story type and create content container
         storyType.classList.add('story-type');
 
-        // const storyContent = document.createElement('div');
         const storyContent = div({ class: 'story-content' });
-        // storyContent.classList.add('story-content');
+        var iconsrc = '';
+        const contentTypeText = contentType.textContent;
+
+        if (contentTypeText === 'podcast-audio') {
+            iconsrc = '/icons/podcast.svg';
+        } else if (contentTypeText === 'video') {
+            iconsrc = '/icons/playbutton.svg'
+        }
+
+        if (iconsrc) {
+            const contentTypeDiv = div({ class: 'contenttype-icon' }, img({ src: iconsrc }));
+            storyContent.appendChild(contentTypeDiv);
+        }
 
         const anchorTag = link.getElementsByTagName('a')[0];
         anchorTag.textContent = '';
