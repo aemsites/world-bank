95ac58cba01b50e2e6ff2af596025f735176dc0f
Mohit Kumar <mohitkumar@adobe.com>
2024-08-24 19:06:40 +0530

    data card authoring fix

--- blocks/data-cards/data-cards.js
+++ blocks/data-cards/data-cards.js
@@ -1,12 +1,11 @@
-function decorateCard(block) {
-  [...block.children].forEach((row) => {
-    row.className = 'data-card';
-    const [tag, title, description, disclaimer] = [...row.children];
-    tag.className = 'data-card-tag';
-    title.className = 'data-card-title';
-    description.className = 'data-card-description';
-    disclaimer.className = 'data-card-disclaimer';
-  });
+function processTag(tag) {
+  const prefix = 'world-bank:';
+  let tagTxt = tag.innerText;
+  if (tagTxt) {
+    tagTxt = tagTxt.replace(prefix, '');
+    tag.classList.add(tagTxt);
+    tag.firstElementChild.innerText = tagTxt;
+  }
 }
 
 /*
@@ -78,5 +77,16 @@ function decorateCard() {
 */
 
 export default async function decorate(block) {
-  decorateCard(block);
+  [...block.children].forEach((row) => {
+    row.className = 'data-card';
+    const [tag, title, description, disclaimer] = [...row.children];
+    tag.className = 'data-card-tag';
+    title.className = 'data-card-title';
+    description.className = 'data-card-description';
+    disclaimer.className = 'data-card-disclaimer';
+
+    if (tag) {
+      processTag(tag);
+    }
+  });
 }
