5f81a15d58cfcac492beba2402007e44d3763c3f
Thomas AS <thomas.jsm13@gmail.com>
2024-08-24 14:33:24 +0530

    return if undefined

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -27,9 +27,10 @@ export default function decorate(block) {
     cards.forEach((card) => {
       // eslint-disable-next-line no-unused-vars
       const [_title, thumbnail, description, ...button] = [...card.children];
-      const picture = thumbnail?.querySelector('picture');
-      thumbnail?.replaceWith(picture);
-      description?.classList.add('columns-feature-card-description');
+      if (!thumbnail || !description) return;
+      const picture = thumbnail.querySelector('picture');
+      thumbnail.replaceWith(picture);
+      description.classList.add('columns-feature-card-description');
 
       const cardContent = div({ class: 'columns-feature-card-content' }, description, ...button);
       const cardGroup = div({ class: 'columns-feature-card-group' }, picture, cardContent);
