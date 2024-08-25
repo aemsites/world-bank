ce96e7d5983f08879866295d103a946af044498c
rpal_elcomp <rpal@estee.com>
2024-08-25 17:15:27 +0530

    WRLDBAEM 62: Css change

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,12 +1,31 @@
-import {
-  getMetadata
-} from '../../scripts/aem.js';
-import {
-  loadFragment
-} from '../fragment/fragment.js';
-import {
-  getLanguage
-} from '../../scripts/scripts.js';
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+import { getLanguage } from '../../scripts/scripts.js';
+
+/**
+ * Switch block handler
+ * @param {HTML} ClassName The container
+ * @param {HTML} ul The content pass in a parent class
+ */
+function switchBlock(className, ul) {
+  const listName = document.createElement('div');
+  listName.className = className;
+  listName.appendChild(ul);
+  return listName;
+}
+
+/**
+ * HTML handler
+ * @param {HTML} ClassName The container
+ * @param {HTML} ul The content pass in a parent class
+ */
+function htmlParser(htmlObj, ul, prepend = '') {
+  [...htmlObj].forEach((Contentdata) => {
+    const li = document.createElement('li');
+    li.appendChild(Contentdata);
+    if (prepend) { ul.prepend(li); } else { ul.append(li); }
+  });
+}
 
 export default async function decorate(block) {
   // load nav as fragment
@@ -15,76 +34,44 @@ export default async function decorate(block) {
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : `/${lang}/footer`;
   const fragment = await loadFragment(footerPath);
 
-
   // decorate nav DOM
   block.textContent = '';
   const section = document.createElement('section');
 
-  let Rows = fragment.firstElementChild.querySelectorAll('.columns-wrapper');
-  let Classes = ['ft-social', 'ft-main', 'ft-legal'];
+  const Rows = fragment.firstElementChild.querySelectorAll('.columns-wrapper');
+  const Classes = ['ft-social', 'ft-main', 'ft-legal'];
   [...Rows].forEach((Rowsdata, iRows) => {
-      Rowsdata.className = Classes[iRows];
+    Rowsdata.className = Classes[iRows];
+    const Columns = Rowsdata.querySelectorAll('[data-block-name="columns"] > div > div');
+    [...Columns].forEach((Columnsdata) => {
+      // Handling "a" tag.
+      const ul = document.createElement('ul');
+      const anchorTagContent = Columnsdata.querySelectorAll('div > p > a');
+      htmlParser(anchorTagContent, ul);
 
-      let Columns = Rowsdata.querySelectorAll('[data-block-name="columns"] > div > div');
+      // Handling "picture" tag.
+      const pictureTagContent = Columnsdata.querySelectorAll('div > picture');
+      htmlParser(pictureTagContent, ul);
 
-      [...Columns].forEach((Columnsdata) => {
+      // Handline "p" tag.
+      const pTagContent = Columnsdata.querySelectorAll('div > p');
+      htmlParser(pTagContent, ul, 'prepend');
 
-          // Handling "a" tag.
-          let ul = document.createElement('ul');
-          let aContent = Columnsdata.querySelectorAll('div > p > a');
-          [...aContent].forEach((Contentdata) => {
-              let li = document.createElement("li");
-              li.appendChild(Contentdata);
-              ul.append(li)
-          }); // aContent
-
-          // Handling "picture" tag.
-          let pictureContent = Columnsdata.querySelectorAll('div > picture');
-          [...pictureContent].forEach((Contentdata) => {
-              let li = document.createElement("li");
-              li.appendChild(Contentdata);
-              ul.append(li);
-          })
-
-          // Handline "p" tag.
-          let pContent = Columnsdata.querySelectorAll('div > p');
-          [...pContent].forEach((Contentdata) => {
-              if (Contentdata.textContent.trim()) {
-                  let li = document.createElement("li");
-                  li.appendChild(Contentdata);
-                  ul.prepend(li)
-              }
-          }); // pContent
-
-          switch (Rowsdata.className) {
-              case 'ft-social':
-                  let socialList = document.createElement('div');
-                  socialList.className = 'ft-social-list';
-                  socialList.appendChild(ul)
-                  Rowsdata.append(socialList)
-                  break;
-
-              case 'ft-main':
-                  let mainList = document.createElement('div');
-                  mainList.className = 'ft-main-item';
-                  mainList.appendChild(ul)
-                  Rowsdata.append(mainList)
-                  break;
-
-              case 'ft-legal':
-                  let legalList = document.createElement('div');
-                  legalList.className = 'ft-legal-list';
-                  legalList.appendChild(ul)
-                  Rowsdata.append(legalList)
-                  break;
-
-              case 'default':
-                  break;
-          }
-      });
-
-      Rowsdata.removeChild(Rowsdata.firstElementChild)
-      section.append(Rowsdata)
+      switch (Rowsdata.className) {
+        case 'ft-social':
+          Rowsdata.append(switchBlock('ft-social-list', ul));
+          break;
+        case 'ft-main':
+          Rowsdata.append(switchBlock('ft-main-item', ul));
+          break;
+        case 'ft-legal':
+          Rowsdata.append(switchBlock('ft-legal-list', ul));
+          break;
+        default:
+      }
+    });
+    Rowsdata.removeChild(Rowsdata.firstElementChild);
+    section.append(Rowsdata);
   });
   block.append(section);
 }
