e54c6ccfeb30ccec64546d8473de51394a2b3624
naina <NAINA>
2024-08-26 15:15:02 +0530

    removed extra spaces

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -10,10 +10,9 @@ export default function decorate(block) {
       if (pic) {
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
           picWrapper.classList.add('columns-img-col');
         }
-      }      
+      }
     });
   });
 
