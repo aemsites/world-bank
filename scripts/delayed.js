857ad4837fb724ed1fdec9bf4f00acbd282deb9e
Mohit Kumar <mohitkumar@adobe.com>
2024-08-26 11:55:40 +0530

    adding body swoosh

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,20 @@
 // add delayed functionality here
+import {
+  getMetadata,
+} from './aem.js';
+
+/**
+ * Swoosh on page
+ */
+function pageSwoosh() {
+  const showSwoosh = getMetadata('show-swoosh');
+  if (showSwoosh) {
+    document.body.classList.add('page-swoosh');
+  }
+}
+
+function loadDelayed() {
+  pageSwoosh();
+}
+
+loadDelayed();
