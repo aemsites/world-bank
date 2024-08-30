11b81ce7176810e9bca383425e6975550c9aab73
naina <NAINA>
2024-08-30 17:41:58 +0530

    ui changes

---
+++ blocks/research-publications-cards/research-publications-cards.js
@@ -0,0 +1,55 @@
+import {
+    div,p,a,button
+  } from '../../scripts/dom-helpers.js';
+
+function createCard(cardData){
+  let cardsContainer= document.querySelector('.cards-container');
+  const [imageContainer,imageAltText,title,desc,titlelink,btn,btnLink] = [...cardData.children];  
+  const imgElement = imageContainer.querySelector('img');
+  imgElement.setAttribute('alt', imageAltText.textContent);
+  imageContainer.className = 'card-img'; 
+  title.className="title";
+  desc.className="desc";
+  btn.className="card-btn";
+
+  var RPCard = div(
+      { class: 'rp-card' }, imageContainer,
+      div({class:"text-content"},a({ href: titlelink.textContent}, title),desc,a({ href: btnLink.textContent}, btn))
+);
+titlelink.remove();
+btnLink.remove();
+imageAltText.remove();
+  if(!cardsContainer){
+   
+   cardsContainer = div({class: 'cards-container'},RPCard);    
+  }
+  else{
+    cardsContainer.append(RPCard)
+  }
+  document.querySelector('.research-publications-cards').append(cardsContainer)
+
+
+}
+
+export default async function decorate(block) {
+      const [title, button,link, rpFirstCard,rpSecondCard] = [...block.children];    
+      title.className= 'title'
+        const titleButtonWrapper = div(
+            { class: 'title-button-wrapper' },
+            title,
+            a({ href: link.textContent ,class:"title-button"}, button)
+      );
+      block.append(titleButtonWrapper)
+const cardsArray =[rpFirstCard,rpSecondCard];
+cardsArray.forEach((cardData)=>{
+  createCard(cardData);
+
+})
+      
+// title.remove();
+// button.remove();
+link.remove();
+// rpFirstCard.remove();
+// rpSecondCard.remove();
+
+    }
