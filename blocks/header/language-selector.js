93b991d67b17dd637ee8cbebb7a1b9c02dff196a
singhpritam18 <pritams@adobe.com>
2024-08-31 16:04:44 +0530

    Updated to add new styles to language selectors

--- blocks/header/language-selector.js
+++ blocks/header/language-selector.js
@@ -10,7 +10,7 @@ function capitalizeFirstLetter(str) {
 }
 // Get Language Selector Display Text based on screen size
 const getLanguageDisplayText = (placeholdersData, lang) => ((window.screen.width >= 768
-    && placeholdersData[`${Constants.LANG_PREFIX}${capitalizeFirstLetter(lang)}`])
+  && placeholdersData[`${Constants.LANG_PREFIX}${capitalizeFirstLetter(lang)}`])
   ? placeholdersData[`${Constants.LANG_PREFIX}${capitalizeFirstLetter(lang)}`]
   : lang);
 
@@ -19,36 +19,18 @@ const updateLanguageTextContent = (domElement, placeholdersData, lang) => {
   domElement.textContent = getLanguageDisplayText(placeholdersData, lang);
 };
 
-// Handle Screen resize for Language Selector ul > li content
-const handleResize = (entries, placeholdersData, language) => {
-  entries.forEach((entry) => {
-    if (entry.target) {
-      const aElement = entry.target.querySelector('a');
-      if (aElement) {
-        updateLanguageTextContent(aElement, placeholdersData, language);
-      }
-    }
-  });
-};
-
-// Create a ResizeObserver instance with a callback
-const createResizeObserver = (placeholdersData, language) => new ResizeObserver((entries) => {
-  handleResize(entries, placeholdersData, language);
-});
-
 // Show/hide Language Selector content on click
 const toggleExpandLanguageSelector = (e) => {
   const toggleContainer = e.currentTarget;
   const toggleContent = toggleContainer.querySelector(Constants.LANGUAGE_CONTENT_SELECTOR);
   if (!toggleContainer || !toggleContent) return;
   const toggler = toggleContainer.querySelector(Constants.LANGUAGE_TOGGLE_SELECTOR);
-  const horizontalLine = toggleContainer.querySelector(Constants.HORIZONTAL_LINE_CLASS_SELECTOR);
   const isExpanded = toggleContainer.classList.contains(Constants.CONTENT_EXPANDED_ACTIVE);
   if (e.type === Constants.TYPE_CLICK) {
     toggleContainer.classList.toggle(Constants.CONTENT_EXPANDED_ACTIVE);
     toggler.setAttribute(Constants.ATTR_ARIA_EXPANDED, !isExpanded);
+    toggler.classList.toggle(Constants.LANGUAGE_TOGGLE_BORDER_CLASS);
     toggleContent.classList.toggle(Constants.CONTENT_EXPANDED);
-    horizontalLine.classList.toggle(Constants.HORIZONTAL_LINE_EXPAND_CLASS);
   }
 };
 
@@ -58,30 +40,15 @@ const fetchLanguageSelectorContent = (placeholdersData, metaLangContent, langCod
     const langPairs = metaLangContent.split(Constants.COMMA_SEPARATOR);
     langPairs.forEach((pair) => {
       const [language, url] = pair.split(Constants.PIPE_SEPARATOR).map((part) => part.trim());
+      if (langCode === language) return;
       const liElement = li(
-        { class: `${langCode === language ? Constants.SHOW_SELECTED_CLASS : ''}` },
         a(
           { href: `${url}` },
-          getLanguageDisplayText(placeholdersData, language),
+          getLanguageDisplayText(placeholdersData, language, false),
         ),
       );
       ulElement.append(liElement);
-      // ResizeObserver for the liElement to show language code for mobile
-      const resizeObserver = createResizeObserver(placeholdersData, language);
-      resizeObserver.observe(liElement);
     });
-  } else {
-    const liElement = li(
-      { class: Constants.SHOW_SELECTED_CLASS },
-      a(
-        { href: window.location.href },
-        getLanguageDisplayText(placeholdersData, langCode),
-      ),
-    );
-    ulElement.append(liElement);
-    // ResizeObserver for the liElement to show language code for mobile
-    const resizeObserver = createResizeObserver(placeholdersData, langCode);
-    resizeObserver.observe(liElement);
   }
   return ulElement;
 };
@@ -89,36 +56,29 @@ const fetchLanguageSelectorContent = (placeholdersData, metaLangContent, langCod
 // Create and return Language Selector DOM Element
 const getLanguageSelector = (placeholdersData, lang) => {
   const metaLangContent = getMetadata(Constants.LANGUAGE_SELECTOR_META_NAME);
-  const languageMap = fetchLanguageSelectorContent(placeholdersData, metaLangContent, lang);
 
-  const languageSelectorContent = div(
-    { class: Constants.LANGUAGE_CONTENT_CLASS },
-    languageMap,
-  );
+  const languageToggle = div({ 'aria-expanded': 'false' });
+  const langSelector = div({ class: Constants.LANGUAGE_CONTAINER_CLASS }, languageToggle);
 
-  const languageToggle = div(
-    {
-      class: Constants.LANAGUAGE_TOGGLE_CLASS,
-      'aria-expanded': 'false',
-    },
-  );
-
-  const langSelector = div(
-    {
-      class: Constants.LANGUAGE_CONTAINER_CLASS,
-      onclick: (e) => toggleExpandLanguageSelector(e),
-    },
-    languageToggle,
-    div({ class: Constants.HORIZONTAL_LINE_CLASS }),
-  );
-  langSelector.append(languageSelectorContent);
-
+  // Show only Current Language when no meta content authored
+  if (!metaLangContent) {
+    updateLanguageTextContent(languageToggle, placeholdersData, lang);
+    languageToggle.classList.add(Constants.LANGUAGE_TEXT_CLASS);
+  } else {
+    const languageMap = fetchLanguageSelectorContent(placeholdersData, metaLangContent, lang);
+    const languageSelectorContent = div(
+      { class: Constants.LANGUAGE_CONTENT_CLASS },
+      languageMap,
+    );
+    languageToggle.classList.add(Constants.LANAGUAGE_TOGGLE_CLASS);
+    langSelector.addEventListener('click', toggleExpandLanguageSelector);
+    langSelector.append(languageSelectorContent);
+  }
   // Show Language code in Mobile
   const resizeObserver = new ResizeObserver(() => {
     updateLanguageTextContent(languageToggle, placeholdersData, lang);
   });
-
-  resizeObserver.observe(langSelector);
+  resizeObserver.observe(document.body);
 
   return langSelector;
 };
