3a70427af452e4d6c0e222d2215d47a6c569f7bc
Mohit Kumar <mohitkumar@adobe.com>
2024-09-01 11:27:41 +0530

    base style cleanup for headings

--- blocks/research-publications-cards/research-publications-cards.js
+++ blocks/research-publications-cards/research-publications-cards.js
@@ -1,12 +1,16 @@
 import { div, a } from '../../scripts/dom-helpers.js';
+import { CLASS_MAIN_HEADING } from '../../scripts/scripts.js';
 
 function createCard(cardData) {
   const [imageContainer, imageAltText, title, desc, titlelink, btn, btnLink] = [
     ...cardData.children,
   ];
-  const imgElement = imageContainer.querySelector('img');
-  imgElement.setAttribute('alt', imageAltText.textContent);
-  imageContainer.className = 'card-img';
+  const imgElement = imageContainer?.querySelector('img');
+  if (imgElement && imageAltText) {
+    imgElement.setAttribute('alt', imageAltText.textContent);
+    imageContainer.className = 'card-img';
+    imageAltText.remove();
+  }
   desc.className = 'desc';
 
   const txtContainer = div(
@@ -20,12 +24,11 @@ function createCard(cardData) {
 
   titlelink.remove();
   btnLink.remove();
-  imageAltText.remove();
 }
 
 export default async function decorate(block) {
   const [title, linkText, link, ...cards] = [...block.children];
-  title.className = 'main-heading';
+  title.className = CLASS_MAIN_HEADING;
   const linkTag = link.getElementsByTagName('a')[0];
   linkTag.innerHTML = linkText.textContent;
   linkTag.title = linkText.textContent;
