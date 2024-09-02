f10ff88ae2613a34fab3b3168026e8c1ce9c1936
Arjun Singh - UG <Arjun.3.Singh@coforge.com>
2024-09-02 18:46:35 +0530

    updating styles

--- blocks/signup/signup.js
+++ blocks/signup/signup.js
@@ -158,9 +158,8 @@ function createSignupModule(block, placeholders) {
       label({
         htmlFor: 'agree',
       }, placeholders[CONSTANTS.SIGNUP_TERMS] || 'I agree with the terms of the Privacy Notice and consent to my personal data being processed, to the extent necessary, to subscribe to the selected updates.'),
-    ),
+    ),div({ class: 'error-message', id: 'term-error-message' }),
     button({ type: 'submit', id: 'signup-btn' }, span({ class: 'icon' }), placeholders[CONSTANTS.SIGNUP_BUTTON_TEXT] || 'Sign up'),
-    div({ class: 'error-message', id: 'term-error-message' }),
   );
 
   formelement.querySelector('label').innerHTML = placeholders[CONSTANTS.SIGNUP_TERMS];
