3ffb8d86a064117263d6ecd4fa89e2dc226a6ba0
Michael Dickson <mdickson@adobe.com>
2024-09-06 15:48:56 -0400

    iterate on css, stub expand/collapse functions

--- blocks/organizational-cards/organizational-cards.js
+++ blocks/organizational-cards/organizational-cards.js
@@ -3,6 +3,7 @@ export default async function decorate(block) {
     console.log(block.children);
 
     const [picEl, ...cards] = block.children;
+    picEl.classList.add('bg-wrapper');
     const bgImg = picEl.querySelector('picture');
     // optimize image
     [...cards].forEach((card) => {
@@ -10,8 +11,11 @@ export default async function decorate(block) {
         card.children[0].classList.add('card-acronym');
         card.children[1].classList.add('card-title');
         card.children[2].classList.add('card-mission');
+        card.children[3].classList.add('card-link-wrapper');
         
+        console.log(`button: ${card.children[3]}`);
         const anchorEl = card.children[3].querySelector('a');
+        console.log(`anchor el: ${anchorEl}`);
         anchorEl.classList.add('card-link');
         //const href = anchorEl.href;
         //const linkText = anchorEl.linkText;
@@ -20,4 +24,12 @@ export default async function decorate(block) {
 
         // process the button
     })
+}
+
+function collapseCard() {
+
+}
+
+function expandCard() {
+
 }
