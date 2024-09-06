638a4c21a9abc7d20f5a1d31e44e243345ae872b
Michael Dickson <mdickson@adobe.com>
2024-09-06 16:28:28 -0400

    add expand/collapse btns/icons

--- blocks/organizational-cards/organizational-cards.js
+++ blocks/organizational-cards/organizational-cards.js
@@ -4,11 +4,12 @@ export default async function decorate(block) {
 
   const [picEl, ...cards] = block.children;
   picEl.classList.add('bg-wrapper');
-  const bgImg = picEl.querySelector('picture');
+  //const bgImg = picEl.querySelector('picture');
   // optimize image
   [...cards].forEach((card) => {
-    card.classList.add('organizational-card');
-    card.children[0].classList.add('card-acronym');
+    card.classList.add('organizational-card', 'collapsed');
+    const acronymEl = card.children[0];
+    acronymEl.classList.add('card-acronym');
     card.children[1].classList.add('card-title');
     card.children[2].classList.add('card-mission');
     card.children[3].classList.add('card-link-wrapper');
@@ -21,6 +22,19 @@ export default async function decorate(block) {
     //const linkText = learnMoreEl.children[1].textContent;
 
     // process the button
+
+    const collapseBtn = document.createElement('div');
+    collapseBtn.classList.add('collapse-btn');
+    const minusIcon = document.createElement('img');
+    minusIcon.src = `${window.hlx.codeBasePath}/icons/icon-minus.svg`;
+    collapseBtn.appendChild(minusIcon);
+    const expandBtn = document.createElement('div');
+    expandBtn.classList.add('expand-btn');
+    const plusIcon = document.createElement('img');
+    plusIcon.src = `${window.hlx.codeBasePath}/icons/icon-plus.svg`;
+    expandBtn.appendChild(plusIcon);
+    acronymEl.appendChild(expandBtn);
+    acronymEl.appendChild(collapseBtn);
   })
 }
 
