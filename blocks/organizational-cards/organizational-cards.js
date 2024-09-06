a8971bb76d122962da1c014ed35de3dd64610c07
Michael Dickson <mdickson@adobe.com>
2024-09-06 14:27:45 -0400

    start tagging card elements

--- blocks/organizational-cards/organizational-cards.js
+++ blocks/organizational-cards/organizational-cards.js
@@ -2,4 +2,26 @@ export default async function decorate(block) {
     console.log('loaded org cards js');
     //const backgroundImg = block.querySelector('picture');
     console.log(block.children);
+    /*
+    [...block.children].forEach((row) => {
+        const picEl = row.querySelector('picture');
+        if (picEl) {
+            // optimize image
+        } else {
+            row.classList.add('organizational-card');
+            // reorganize link
+        }
+    })
+    */
+    [picEl, ...cards] = block.children;
+    const bgImg = picEl.querySelector('picture');
+    // optimize image
+    [...cards].forEach((card) => {
+        card.classList.add('organizational-card');
+        card.children[0].classList.add('card-acronym');
+        card.children[1].classList.add('card-title');
+        card.children[2].classList.add('card-mission');
+        
+        const learnMore = card.children[3];
+    })
 }
