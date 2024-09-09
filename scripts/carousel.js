3a2695cbf7efa36a9232c392237119a40e4260b1
rpal_elcomp <rpal@estee.com>
2024-09-09 10:11:42 +0530

    WRLDBAEMS-87: Carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -42,61 +42,61 @@ export default async function createCarousel(block) {
       threshold: 0.25,
     };
 
-  // Button Event Handler
-  moveRightBtn.addEventListener('click', () => {
-    carouselItems.scrollLeft += itemWidth;
-  }, false);
+    // Button Event Handler
+    moveRightBtn.addEventListener('click', () => {
+      carouselItems.scrollLeft += itemWidth;
+    }, false);
 
-  moveLeftBtn.addEventListener('click', () => {
-    carouselItems.scrollLeft -= itemWidth;
-  }, false);
+    moveLeftBtn.addEventListener('click', () => {
+      carouselItems.scrollLeft -= itemWidth;
+    }, false);
 
-  // Observer Callback Function
-  const callBack = (entries) => {
-    const dir = document.documentElement.dir || 'ltr';
+    // Observer Callback Function
+    const callBack = (entries) => {
+      const dir = document.documentElement.dir || 'ltr';
 
-    if (dir === 'rtl') {
-      document.querySelector('.next').style.right = 'auto';
-      document.querySelector('.prev').style.right = 'auto';
-      document.querySelector('.next').style.left = '90px';
-      document.querySelector('.prev').style.left = '90px';
-    }
-
-    entries.forEach((entry) => {
-      const {
-        target,
-      } = entry;
-      if (entry.intersectionRatio >= 0.25) {
-        target.classList.remove('opacity');
-        target.classList.add('active');
-      } else {
-        target.classList.remove('active');
-        target.classList.add('opacity');
+      if (dir === 'rtl') {
+        document.querySelector('.next').style.right = 'auto';
+        document.querySelector('.prev').style.right = 'auto';
+        document.querySelector('.next').style.left = '90px';
+        document.querySelector('.prev').style.left = '90px';
       }
+
+      entries.forEach((entry) => {
+        const {
+          target,
+        } = entry;
+        if (entry.intersectionRatio >= 0.25) {
+          target.classList.remove('opacity');
+          target.classList.add('active');
+        } else {
+          target.classList.remove('active');
+          target.classList.add('opacity');
+        }
+      });
+
+      try {
+        if (entries[0].target.parentElement.children[0].className === 'active') {
+          moveLeftBtn.style.display = dir === 'ltr' ? 'none' : 'block';
+          moveRightBtn.style.display = dir === 'ltr' ? 'block' : 'none';
+        } else if (entries[0].target.parentElement.children[entries[0].target.parentElement.children.length - 1].className === 'active') {
+          moveLeftBtn.style.display = dir === 'ltr' ? 'block' : 'none';
+          moveRightBtn.style.display = dir === 'ltr' ? 'none' : 'block';
+        } else {
+          moveLeftBtn.style.display = 'block';
+          moveRightBtn.style.display = 'block';
+        }
+      } catch (e) {
+        /* error structure was not as expected */
+      }
+    };
+
+    // Create Observer instance
+    const observer = new IntersectionObserver(callBack, observerOptions);
+
+    // Apply observer on each item
+    itemList.forEach((item) => {
+      observer.observe(item);
     });
-
-    try {
-      if (entries[0].target.parentElement.children[0].className === 'active') {
-        moveLeftBtn.style.display = dir === 'ltr' ? 'none' : 'block';
-        moveRightBtn.style.display = dir === 'ltr' ? 'block' : 'none';
-      } else if (entries[0].target.parentElement.children[entries[0].target.parentElement.children.length - 1].className === 'active') {
-        moveLeftBtn.style.display = dir === 'ltr' ? 'block' : 'none';
-        moveRightBtn.style.display = dir === 'ltr' ? 'none' : 'block';
-      } else {
-        moveLeftBtn.style.display = 'block';
-        moveRightBtn.style.display = 'block';
-      }
-    } catch (e) {
-      /* error structure was not as expected */
-    }
-  };
-
-  // Create Observer instance
-  const observer = new IntersectionObserver(callBack, observerOptions);
-
-  // Apply observer on each item
-  itemList.forEach((item) => {
-    observer.observe(item);
-  });
   }, 100);
 }
