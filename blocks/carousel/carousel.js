d2d7d0b3e5adb6697c9acadc1e91636385a07e64
rpal_elcomp <rpal@estee.com>
2024-09-10 19:57:55 +0530

    WRLDBAEMS-87: Carousel implementation

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -4,8 +4,8 @@ import createSlider from '../../scripts/slider.js';
 
 export default function decorate(block) {
   let i = 0;
-  const ul = document.createElement('ul');
-  const divtag = document.createElement('div');
+  const slider = document.createElement('ul');
+  const left_content = document.createElement('div');
   [...block.children].forEach((row) => {
     if (i > 3) {
       const li = document.createElement('li');
@@ -15,26 +15,26 @@ export default function decorate(block) {
         if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
         else div.className = 'cards-card-body';
       });
-      ul.append(li);
-    } else if (i <= 3) {
+      slider.append(li);
+    } else {
       if (row.firstElementChild.firstElementChild) {
-        divtag.append(row.firstElementChild.firstElementChild);
+        left_content.append(row.firstElementChild.firstElementChild);
       }
       if (row.firstElementChild) {
-        divtag.append(row.firstElementChild.firstElementChild || '');
+        left_content.append(row.firstElementChild.firstElementChild || '');
       }
-      divtag.className = 'default-content-wrapper';
+      left_content.className = 'default-content-wrapper';
     }
     i += 1;
   });
 
-  ul.querySelectorAll('picture > img').forEach((img) => {
+  slider.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
   block.textContent = '';
-  block.parentNode.parentNode.prepend(divtag);
-  block.append(ul);
+  block.parentNode.parentNode.prepend(left_content);
+  block.append(slider);
   createSlider(block);
 }
