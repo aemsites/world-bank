7e69fb6a668bab9009d07eb5db66d28412e6695d
Samriddhi-1998 <samriddhig4@gmail.com>
2024-09-11 15:52:08 +0530

    updated eslint to add 5 cells to data and news card

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,8 @@ module.exports = {
       {
         '*': 4,
         'curated-card': 7,
+        'data-card': 5,
+        'news-card': 5,
         hero: 5,
         'explore-card': 7,
         section: 7,
