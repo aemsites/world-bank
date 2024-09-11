7e10823b2061676e6b6e33471f33e314b34de6a4
Samriddhi-1998 <samriddhig4@gmail.com>
2024-09-11 16:06:41 +0530

    added data and news card link

--- blocks/data-cards/data-cards.js
+++ blocks/data-cards/data-cards.js
@@ -1,10 +1,10 @@
 import { processTags } from '../../scripts/utils.js';
-import { div } from '../../scripts/dom-helpers.js';
+import { div, a } from '../../scripts/dom-helpers.js';
 
 function processTag(tag) {
   let tagTxt = tag.innerText;
   if (tagTxt) {
-    tagTxt = processTags(tagTxt, '');
+    tagTxt = processTags(tagTxt, 'category');
     tag.classList.add(tagTxt);
     // TODO: Read it from placeholder
     tag.firstElementChild.innerText = tagTxt;
@@ -21,6 +21,7 @@ export default async function decorate(block) {
   const isDataCardVariation = block.classList.contains('data-card-variation');
   if (isDataCardVariation) {
     const [rte1, rte2, rte3, ...cards] = [...block.children];
+    rte1.className = 'main-heading';
     if (rte1 && rte2 && rte3) {
       block.append(createRTEContainer([rte1, rte2, rte3]));
     }
@@ -33,11 +34,16 @@ export default async function decorate(block) {
 
     cards.forEach((row) => {
       row.className = 'data-card';
-      const [tag, title, description, disclaimer] = [...row.children];
+      const [tag, title, description, link, disclaimer] = [...row.children];
       tag.className = 'data-card-tag';
       title.className = 'data-card-title';
       description.className = 'data-card-description';
       disclaimer.className = 'data-card-disclaimer';
+      const anchor = a({ class: 'anchor-tag', href: link.textContent });
+      anchor.append(title);
+      anchor.append(description);
+      row.append(anchor);
+      row.append(disclaimer);
 
       if (tag) {
         processTag(tag);
@@ -48,6 +54,7 @@ export default async function decorate(block) {
   const isNewsCardVariation = block.classList.contains('news-card-variation');
   if (isNewsCardVariation) {
     const [rte1, rte2, rte3, ...cards] = [...block.children];
+    rte1.className = 'main-heading';
     if (rte1 && rte2 && rte3) {
       block.append(createRTEContainer([rte1, rte2, rte3]));
     }
@@ -60,10 +67,14 @@ export default async function decorate(block) {
 
     cards.forEach((row) => {
       row.className = 'news-card';
-      const [tag, title, image] = [...row.children];
+      const [tag, title, link, image] = [...row.children];
       tag.className = 'news-card-tag';
       title.className = 'news-card-title';
       image.className = 'news-card-image';
+      const anchor = a({ class: 'anchor-tag', href: link.textContent });
+      anchor.append(title.querySelector('p'));
+      title.append(anchor);
+
 
       if (tag) {
         processTag(tag);
