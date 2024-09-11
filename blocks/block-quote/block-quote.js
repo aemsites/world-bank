50104e8c6edbbe033c14b138136f1c31f2ec9226
singhpritam18 <pritams@adobe.com>
2024-09-11 19:15:16 +0530

    removed JS file

--- blocks/block-quote/block-quote.js
+++
@@ -1,9 +0,0 @@
-import { div } from '../../scripts/dom-helpers.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-export default function decorate(block) {
-  console.log(block.children);
-  const pTag = block.querySelector('p');
-  block.appendChild(pTag);
-  moveInstrumentation(pTag, block);
-
-}
