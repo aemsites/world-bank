908364903649ea416f8456994cc5d204b55ac018
Michael Dickson <mdickson@adobe.com>
2024-09-13 15:40:37 -0400

    linting

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -72,7 +72,7 @@ async function loadFonts() {
 function openPopUp(popUrl) {
   const popupParams = `height=450, width=550, top=${(window.innerHeight / 2 - 275)}`
    + `, left=${(window.innerWidth / 2 - 225)}`
-   + `, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0`;
+   + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0';
   window.open(popUrl, 'fbShareWindow', popupParams);
 }
 
