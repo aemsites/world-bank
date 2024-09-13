474b347695de8b3e93c76583ef2b825d7a8bb1d0
singhpritam18 <pritams@adobe.com>
2024-09-13 16:42:44 +0530

    WIP: added CSS and JS changes

--- blocks/measuring-impact-cards/measuring-impact-cards.js
+++ blocks/measuring-impact-cards/measuring-impact-cards.js
@@ -1,16 +1,32 @@
 import { div, a, p } from '../../scripts/dom-helpers.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
+import { moveInstrumentation, fetchLanguagePlaceholders } from '../../scripts/scripts.js';
 
-function processImpactCard(impactCard) {
+const getShortendIndicatorName = (indicatorTag, placeholdersData) => {
+  // Return Same indicator tag content if hyphen is not present
+  if (indicatorTag && indicatorTag.indexOf('-') < 0) return indicatorTag;
+
+  const indicatorKey = indicatorTag.split('-')[0].trim().replace(/[^a-zA-Z0-9]/g, '');
+  return placeholdersData[indicatorKey] ? placeholdersData[indicatorKey] : indicatorKey;
+};
+
+const getColorCode = (indicatorTag, placeholdersData) => {
+  if (indicatorTag && indicatorTag.indexOf('-') < 0) return '#07AB50;';
+  const indicatorColorKey = indicatorTag.split('-')[1].trim().replace(/[^a-zA-Z0-9]/g, '');
+  return placeholdersData[indicatorColorKey] ? placeholdersData[indicatorColorKey]
+    : indicatorColorKey;
+};
+
+const getImpactCardWrapper = (impactCard, placeholdersData) => {
   const [indicatorTag, icon, title, description, cardLink] = [...impactCard.children];
-  const impactCardWrapper = div({ class: 'impact-card-wrapper' });
+  const colorCode = getColorCode(indicatorTag.textContent, placeholdersData);
+  const impactCardWrapper = div({ class: 'impact-card-wrapper', style: `border-top: 2px solid ${colorCode}` });
   moveInstrumentation(impactCard, impactCardWrapper);
 
   const impactCardIcon = div({ class: 'impact-card-img' });
   const impactCardContent = div(
     { class: 'impact-card-content' },
-    p({ class: 'impact-card-title' }, title.textContent),
-    p({ class: 'impact-card-description' }, description.textContent),
+    p(title.textContent),
+    p(description.textContent),
   );
 
   const link = cardLink.textContent ? cardLink.textContent : '#';
@@ -25,28 +41,25 @@ function processImpactCard(impactCard) {
     impactCardContent,
   );
 
-//   impactCardWrapper.append(a(
-//     { href: link },
-//     p({ class: 'impact-indicator' }, indicatorTag.textContent),
-//     impactCardBody,
-//   ));
-
-  impactCardWrapper.append(
-    p({ class: 'impact-indicator' }, indicatorTag.textContent),
+  impactCardWrapper.append(a(
+    { href: link },
+    p({ class: 'impact-indicator' }, getShortendIndicatorName(indicatorTag.textContent, placeholdersData)),
     impactCardBody,
-  );
+  ));
 
   // Remove default one's
   impactCard.innerHTML = '';
   impactCard.remove();
 
   return impactCardWrapper;
-}
+};
+
 export default async function decorate(block) {
   const impactCardList = Array.from(block.children);
   if (impactCardList && impactCardList.length > 0) {
+    const placeholdersData = await fetchLanguagePlaceholders();
     impactCardList.forEach((impactCard) => {
-      const impactCardContentWrapper = processImpactCard(impactCard);
+      const impactCardContentWrapper = getImpactCardWrapper(impactCard, placeholdersData);
       block.append(impactCardContentWrapper);
     });
   }
