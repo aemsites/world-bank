f24c49c670779872ccce38718f97703116bf3375
padmashree <padmashreeshetty245@gmail.com>
2024-09-14 11:59:32 +0530

    [fix] lint

--- blocks/priority-list/priority-list.js
+++ blocks/priority-list/priority-list.js
@@ -1,6 +1,6 @@
-import { div, img } from "../../scripts/dom-helpers.js";
-import { CLASS_MAIN_HEADING } from "../../scripts/scripts.js";
-import { TAG_ROOT } from "../../scripts/utils.js";
+import { div, img } from '../../scripts/dom-helpers.js';
+import { CLASS_MAIN_HEADING } from '../../scripts/scripts.js';
+import { TAG_ROOT } from '../../scripts/utils.js';
 
 const desktopConst = 400;
 const tabConst = 150;
@@ -12,14 +12,14 @@ function createCard(card) {
   }
   cardtitle.textContent = cardtitle.textContent.replace(
     `${TAG_ROOT}category/`,
-    ""
+    '',
   );
   card.className = cardtitle.textContent;
-  cardtitle.className = "cardtitle";
-  carddesc.className = "carddesc";
+  cardtitle.className = 'cardtitle';
+  carddesc.className = 'carddesc';
 }
 function handlePriorityListScroll(leftColumnContainer, cards) {
-  const image = leftColumnContainer.querySelector(".image-container");
+  const image = leftColumnContainer.querySelector('.image-container');
   if (image.getBoundingClientRect().top === tabTopPosition) {
     let prevmin;
     let prevval;
@@ -37,7 +37,7 @@ function handlePriorityListScroll(leftColumnContainer, cards) {
     });
     cards.forEach((card, index) => {
       if (index === prevval) {
-        image.style.backgroundImage = `url(${card.querySelector("img").src})`;
+        image.style.backgroundImage = `url(${card.querySelector('img').src})`;
       }
     });
   } else if (image.getBoundingClientRect().top >= 0) {
@@ -46,8 +46,8 @@ function handlePriorityListScroll(leftColumnContainer, cards) {
     cards.forEach((card, index) => {
       const curtop = card.getBoundingClientRect().top;
       if (
-        image.getBoundingClientRect().top >=
-        card.getBoundingClientRect().top - desktopConst
+        image.getBoundingClientRect().top
+        >= card.getBoundingClientRect().top - desktopConst
       ) {
         if (index === 0) {
           prevmin = curtop;
@@ -60,7 +60,7 @@ function handlePriorityListScroll(leftColumnContainer, cards) {
     });
     cards.forEach((card, index) => {
       if (index === prevval) {
-        image.style.backgroundImage = `url(${card.querySelector("img").src})`;
+        image.style.backgroundImage = `url(${card.querySelector('img').src})`;
       }
     });
   }
@@ -70,41 +70,41 @@ export default async function decorate(block) {
     ...block.children,
   ];
   if (
-    !subtitle ||
-    !maintitle ||
-    !buttontext ||
-    !buttonlink ||
-    cards.length === 0
+    !subtitle
+    || !maintitle
+    || !buttontext
+    || !buttonlink
+    || cards.length === 0
   ) {
     return;
   }
-  subtitle.className = "subtitle";
+  subtitle.className = 'subtitle';
   maintitle.className = CLASS_MAIN_HEADING;
-  const buttonLink = buttonlink.getElementsByTagName("a")[0];
+  const buttonLink = buttonlink.getElementsByTagName('a')[0];
   buttonLink.innerHTML = buttontext.textContent;
   buttonLink.title = buttontext.textContent;
-  buttonLink.className = "button primary";
+  buttonLink.className = 'button primary';
   const buttonWrapper = div(
-    { class: "button-wrapper" },
-    buttonLink.cloneNode(true)
+    { class: 'button-wrapper' },
+    buttonLink.cloneNode(true),
   );
-  const textContainer = div({ class: "text-container" });
+  const textContainer = div({ class: 'text-container' });
   textContainer.append(subtitle);
   textContainer.append(maintitle);
   textContainer.append(buttonWrapper);
   buttonlink.remove();
   buttontext.remove();
-  const cardsContainer = div({ class: "cards-container" });
+  const cardsContainer = div({ class: 'cards-container' });
   cards.forEach((card) => {
     createCard(card);
     cardsContainer.append(card);
   });
-  const leftColumnContainer = div({ class: "left-column-container" });
-  const rightColumnContainer = div({ class: "right-column-container" });
+  const leftColumnContainer = div({ class: 'left-column-container' });
+  const rightColumnContainer = div({ class: 'right-column-container' });
   rightColumnContainer.append(textContainer);
   rightColumnContainer.append(cardsContainer);
-  const imageContainer = div({ class: "image-container" });
-  const firstimg = cards.at(0).querySelector("img");
+  const imageContainer = div({ class: 'image-container' });
+  const firstimg = cards.at(0).querySelector('img');
   if (firstimg) {
     const imgElement = img({ src: firstimg.src });
     imageContainer.append(imgElement);
@@ -113,8 +113,6 @@ export default async function decorate(block) {
   leftColumnContainer.append(imageContainer);
   block.append(leftColumnContainer);
 
-  window.addEventListener("scroll", () =>
-    handlePriorityListScroll(leftColumnContainer, cards)
-  );
+  window.addEventListener('scroll', () => handlePriorityListScroll(leftColumnContainer, cards));
   block.append(rightColumnContainer);
 }
