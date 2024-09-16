81027a4bf2484916f553830197dcc0f10083e47a
rpal_elcomp <rpal@estee.com>
2024-09-16 14:09:52 +0530

    WRLDBAEMS-274: Carousel Feedback

--- scripts/slider.js
+++ scripts/slider.js
@@ -34,13 +34,13 @@ export default async function createSlider(block) {
   block.append(arrow(`${prevBtn}`));
 
   // Call function after page load
-    const moveRightBtn = document.querySelector(`.${nextBtn}`);
-    const moveLeftBtn = document.querySelector(`.${prevBtn}`);
-    const itemList = [...document.querySelectorAll('.carousel > ul > li')];
-    const observerOptions = {
-      rootMargin: '0px',
-      threshold: 0.25,
-    };
+  const moveRightBtn = document.querySelector(`.${nextBtn}`);
+  const moveLeftBtn = document.querySelector(`.${prevBtn}`);
+  const itemList = [...document.querySelectorAll('.carousel > ul > li')];
+  const observerOptions = {
+    rootMargin: '0px',
+    threshold: 0.25,
+  };
 
   function moveDirection(itemWidth, option) {
     const carouselItems = document.querySelector('.carousel > ul');
@@ -68,60 +68,60 @@ export default async function createSlider(block) {
 
     // Observer Callback Function
     const callBack = (entries) => {
-      const dir = document.documentElement.dir || 'ltr';
-      let disableLeftBtn = false;
-      let disableRightBtn = false;
+    const dir = document.documentElement.dir || 'ltr';
+    let disableLeftBtn = false;
+    let disableRightBtn = false;
 
-      if (dir === 'rtl') {
-        document.querySelector('.next').style.right = 'auto';
-        document.querySelector('.prev').style.right = 'auto';
-        document.querySelector('.next').style.left = '0';
-        document.querySelector('.prev').style.left = '0';
+    if (dir === 'rtl') {
+      document.querySelector('.next').style.right = 'auto';
+      document.querySelector('.prev').style.right = 'auto';
+      document.querySelector('.next').style.left = '0';
+      document.querySelector('.prev').style.left = '0';
+    }
+
+    entries.forEach((entry) => {
+      const {
+        target,
+      } = entry;
+      if (entry.intersectionRatio >= 0.25) {
+        target.classList.remove('opacity');
+        target.classList.add('active');
+      } else {
+        target.classList.remove('active');
+        target.classList.add('opacity');
       }
-
-      entries.forEach((entry) => {
-        const {
-          target,
-        } = entry;
-        if (entry.intersectionRatio >= 0.25) {
-          target.classList.remove('opacity');
-          target.classList.add('active');
-        } else {
-          target.classList.remove('active');
-          target.classList.add('opacity');
-        }
-      });
-
-      try {
-        if (entries[0].target.parentElement.children[0].className === 'active') {
-          if (dir === 'rtl') {
-            disableLeftBtn = false;
-            disableRightBtn = true;
-          } else {
-            disableLeftBtn = true;
-            disableRightBtn = false;
-          }
-        } else if (entries[0].target.parentElement.children[entries[0].target.parentElement.children.length - 1].className === 'active') {
-          if (dir === 'rtl') {
-            disableLeftBtn = true;
-            disableRightBtn = false;
-          } else {
-            disableLeftBtn = false;
-            disableRightBtn = true;
-          }
-        }
-        moveLeftBtn.disabled = disableLeftBtn;
-        moveRightBtn.disabled = disableRightBtn;
-      } catch (e) {
-        /* error structure was not as expected */
-      }
-    };
-
-    // Create Observer instance
-    const observer = new IntersectionObserver(callBack, observerOptions);
-
-    // Apply observer on each item
-    itemList.forEach((item) => {
-      observer.observe(item);
     });
+
+    try {
+      if (entries[0].target.parentElement.children[0].className === 'active') {
+        if (dir === 'rtl') {
+          disableLeftBtn = false;
+          disableRightBtn = true;
+        } else {
+          disableLeftBtn = true;
+          disableRightBtn = false;
+        }
+      } else if (entries[0].target.parentElement.children[entries[0].target.parentElement.children.length - 1].className === 'active') {
+        if (dir === 'rtl') {
+          disableLeftBtn = true;
+          disableRightBtn = false;
+        } else {
+          disableLeftBtn = false;
+          disableRightBtn = true;
+        }
+      }
+      moveLeftBtn.disabled = disableLeftBtn;
+      moveRightBtn.disabled = disableRightBtn;
+    } catch (e) {
+      /* error structure was not as expected */
+    }
+  };
+
+  // Create Observer instance
+  const observer = new IntersectionObserver(callBack, observerOptions);
+
+  // Apply observer on each item
+  itemList.forEach((item) => {
+    observer.observe(item);
+  });
 }
