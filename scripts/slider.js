c47507a02c9ceb29e060f0d642d32e9f021e663f
adobe-rpal <tec28425@adobe.com>
2024-09-16 15:43:09 +0530

    WRLDBAEMS-274: Carousel Feedback (#222)
    * WRLDBAEMS-274: Carousel Feedback
    
    ---------
    
    Co-authored-by: rpal_elcomp <rpal@estee.com>
    Co-authored-by: Thomas Sebastian <thomas.jsm13@gmail.com>

--- scripts/slider.js
+++ scripts/slider.js
@@ -34,84 +34,94 @@ export default async function createSlider(block) {
   block.append(arrow(`${prevBtn}`));
 
   // Call function after page load
-  window.setTimeout(() => {
+  const moveRightBtn = document.querySelector(`.${nextBtn}`);
+  const moveLeftBtn = document.querySelector(`.${prevBtn}`);
+  const itemList = [...document.querySelectorAll('.carousel > ul > li')];
+  const observerOptions = {
+    rootMargin: '0px',
+    threshold: 0.25,
+  };
+
+  function moveDirection(itemWidth, option) {
+    const carouselItems = document.querySelector('.carousel > ul');
+    if (option === '+') {
+      carouselItems.scrollLeft += itemWidth;
+    } else {
+      carouselItems.scrollLeft -= itemWidth;
+    }
+  }
+
+  // Button Event Handler
+  moveLeftBtn.addEventListener('click', () => {
     const carouselItems = document.querySelector('.carousel > ul');
     const totalItems = carouselItems.children.length || 1;
-    const moveRightBtn = document.querySelector(`.${nextBtn}`);
-    const moveLeftBtn = document.querySelector(`.${prevBtn}`);
     const itemWidth = parseInt(carouselItems.scrollWidth / totalItems, 10);
-    const itemList = [...document.querySelectorAll('.carousel > ul > li')];
-    const observerOptions = {
-      rootMargin: '0px',
-      threshold: 0.25,
-    };
+    moveDirection(itemWidth, '-');
+  }, true);
 
-    // Button Event Handler
-    moveRightBtn.addEventListener('click', () => {
-      carouselItems.scrollLeft += itemWidth;
-    }, false);
+  moveRightBtn.addEventListener('click', () => {
+    const carouselItems = document.querySelector('.carousel > ul');
+    const totalItems = carouselItems.children.length || 1;
+    const itemWidth = parseInt(carouselItems.scrollWidth / totalItems, 10);
+    moveDirection(itemWidth, '+');
+  }, true);
 
-    moveLeftBtn.addEventListener('click', () => {
-      carouselItems.scrollLeft -= itemWidth;
-    }, false);
+  // Observer Callback Function
+  const callBack = (entries) => {
+    const dir = document.documentElement.dir || 'ltr';
+    let disableLeftBtn = false;
+    let disableRightBtn = false;
 
-    // Observer Callback Function
-    const callBack = (entries) => {
-      const dir = document.documentElement.dir || 'ltr';
-      let disableLeftBtn = false;
-      let disableRightBtn = false;
+    if (dir === 'rtl') {
+      document.querySelector('.next').style.right = 'auto';
+      document.querySelector('.prev').style.right = 'auto';
+      document.querySelector('.next').style.left = '0';
+      document.querySelector('.prev').style.left = '0';
+    }
 
-      if (dir === 'rtl') {
-        document.querySelector('.next').style.right = 'auto';
-        document.querySelector('.prev').style.right = 'auto';
-        document.querySelector('.next').style.left = '0';
-        document.querySelector('.prev').style.left = '0';
+    entries.forEach((entry) => {
+      const {
+        target,
+      } = entry;
+      if (entry.intersectionRatio >= 0.25) {
+        target.classList.remove('opacity');
+        target.classList.add('active');
+      } else {
+        target.classList.remove('active');
+        target.classList.add('opacity');
       }
-
-      entries.forEach((entry) => {
-        const {
-          target,
-        } = entry;
-        if (entry.intersectionRatio >= 0.25) {
-          target.classList.remove('opacity');
-          target.classList.add('active');
-        } else {
-          target.classList.remove('active');
-          target.classList.add('opacity');
-        }
-      });
-
-      try {
-        if (entries[0].target.parentElement.children[0].className === 'active') {
-          if (dir === 'rtl') {
-            disableLeftBtn = false;
-            disableRightBtn = true;
-          } else {
-            disableLeftBtn = true;
-            disableRightBtn = false;
-          }
-        } else if (entries[0].target.parentElement.children[entries[0].target.parentElement.children.length - 1].className === 'active') {
-          if (dir === 'rtl') {
-            disableLeftBtn = true;
-            disableRightBtn = false;
-          } else {
-            disableLeftBtn = false;
-            disableRightBtn = true;
-          }
-        }
-        moveLeftBtn.disabled = disableLeftBtn;
-        moveRightBtn.disabled = disableRightBtn;
-      } catch (e) {
-        /* error structure was not as expected */
-      }
-    };
-
-    // Create Observer instance
-    const observer = new IntersectionObserver(callBack, observerOptions);
-
-    // Apply observer on each item
-    itemList.forEach((item) => {
-      observer.observe(item);
     });
-  }, 0);
+
+    try {
+      if (entries[0].target.parentElement.children[0].className === 'active') {
+        if (dir === 'rtl') {
+          disableLeftBtn = false;
+          disableRightBtn = true;
+        } else {
+          disableLeftBtn = true;
+          disableRightBtn = false;
+        }
+      } else if (entries[0].target.parentElement.children[entries[0].target.parentElement.children.length - 1].className === 'active') {
+        if (dir === 'rtl') {
+          disableLeftBtn = true;
+          disableRightBtn = false;
+        } else {
+          disableLeftBtn = false;
+          disableRightBtn = true;
+        }
+      }
+      moveLeftBtn.disabled = disableLeftBtn;
+      moveRightBtn.disabled = disableRightBtn;
+    } catch (e) {
+      /* error structure was not as expected */
+    }
+  };
+
+  // Create Observer instance
+  const observer = new IntersectionObserver(callBack, observerOptions);
+
+  // Apply observer on each item
+  itemList.forEach((item) => {
+    observer.observe(item);
+  });
 }
