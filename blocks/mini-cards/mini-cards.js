52be8dbce34da90d884bc9b390f6d1760e553c76
padmashree <padmashreeshetty245@gmail.com>
2024-09-26 11:10:40 +0530

    Js added to handle alt text in mini-card

--- blocks/mini-cards/mini-cards.js
+++ blocks/mini-cards/mini-cards.js
@@ -21,7 +21,7 @@ export default function decorate(block) {
   const miniCardsContainer = div({ class: 'mini-card-container' });
   cards.forEach((row) => {
     row.className = 'mini-card';
-    const [imageDiv, tagDiv, titleDiv, dateDiv, timeDiv, locationDiv, linkDiv] = row.children;
+    const [imageDiv, alt, tagDiv, titleDiv, dateDiv, timeDiv, locationDiv, linkDiv] = row.children;
     imageDiv.className = 'mini-card-image';
     tagDiv.className = 'mini-card-tag';
     titleDiv.className = 'mini-card-title';
@@ -29,6 +29,11 @@ export default function decorate(block) {
     timeDiv.className = 'mini-card-time';
     locationDiv.className = 'mini-card-location';
     const link = linkDiv.textContent ? linkDiv.textContent : '';
+    if (alt) {
+      const pic = imageDiv.querySelector('img');
+      pic.alt = alt.querySelector('p').textContent.trim();
+      alt.remove();
+    }
     linkDiv.remove();
     if (!timeDiv.textContent) {
       timeDiv.remove();
