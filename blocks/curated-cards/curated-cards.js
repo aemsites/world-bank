4475bd30e96245e599845dab576fc55ab7ad79ec
padmashree <padmashreeshetty245@gmail.com>
2024-09-26 15:33:21 +0530

    lint

--- blocks/curated-cards/curated-cards.js
+++ blocks/curated-cards/curated-cards.js
@@ -28,7 +28,9 @@ function createFeatureCard(row, placeHolders) {
   const featureDiv = div({ class: 'feature-card' });
   moveInstrumentation(row, featureDiv);
   featureTagContent.innerHTML = '';
-  featureAltContent.innerHTML = '';
+  if (featureAltContent) {
+    featureAltContent.innerHTML = '';
+  }
   const featureContentWrapper = div(
     { class: 'feature-card-content' },
     div({ class: ' feature-card-content-text' }, a({ href: featureLink.textContent }, h1({ class: 'feature-card-content-heading' }, featureHeadingContent.textContent), p({ class: 'feature-card-content-description' }, featureDescContent.textContent))),
