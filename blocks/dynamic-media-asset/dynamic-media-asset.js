6588f3389ef02e1195e90f1d0465a266df332db0
Rob Rusher <rrusher@adobe.com>
2024-10-01 11:38:57 -0600

    update decorate

--- blocks/dynamic-media-asset/dynamic-media-asset.js
+++ blocks/dynamic-media-asset/dynamic-media-asset.js
@@ -3,5 +3,5 @@ import { img } from '../../scripts/dom-helpers.js';
 export default async function decorate(block) {
   const href = block.querySelector('a').getAttribute('href');
   block.innerHTML = '';
-  block.append(img(href));
+  block.append(img({ src: href }));
 }
