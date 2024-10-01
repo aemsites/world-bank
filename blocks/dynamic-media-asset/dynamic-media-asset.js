7343fa1be1f91acd05efe74daceb77087dda4b27
Rob Rusher <rrusher@adobe.com>
2024-10-01 11:48:07 -0600

    get alt

--- blocks/dynamic-media-asset/dynamic-media-asset.js
+++ blocks/dynamic-media-asset/dynamic-media-asset.js
@@ -2,6 +2,7 @@ import { img } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
   const href = block.querySelector('a').getAttribute('href');
+  const altText = block.querySelector('p[data-aue-prop="dm_alttext"]').textContent;
   block.innerHTML = '';
-  block.append(img({ src: href }));
+  block.append(img({ src: href, alt: altText }));
 }
