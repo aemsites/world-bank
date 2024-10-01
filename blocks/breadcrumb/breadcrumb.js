b3d95938ffed507d99d8f06f0d37c4247b51e079
Akash Gupta <akasgupta@adobe.com>
2024-10-01 20:32:29 +0530

    hide current page from breadcrumb and added schema html

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -2,51 +2,27 @@ import { fetchSearch, LANGUAGE_ROOT } from '../../scripts/scripts.js';
 import {
   ol, li, a, span,
 } from '../../scripts/dom-helpers.js';
-import { getMetadata } from '../../scripts/aem.js';
-
-/**
- * Get label for the current page. If page does not exist in search index, get the label
- * from og:title metadata or else get it from the page url in title case format.
- * @param {*} searchIndex
- * @returns label
- */
-const getCurrentPageLabel = (searchIndex) => {
-  const currentPagePath = window.location.pathname;
-  let label = '';
-  const pageObj = searchIndex.filter((item) => item.path === currentPagePath);
-  if (pageObj && pageObj.length === 1) {
-    label = pageObj[0].navTitle ? pageObj[0].navTitle : pageObj[0].title;
-    if (label) {
-      return label;
-    }
-  }
-  label = getMetadata('og:title');
-  if (label) {
-    return label;
-  }
-  const currentPageName = currentPagePath.split('/').pop();
-  return currentPageName.toLowerCase().split(' ')
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-    .replace(/-/g, ' ');
-};
 
 export default async function decorate(block) {
   const results = await fetchSearch();
   const regex = '^/(.*?)/?$';
   const pathSegments = window.location.pathname.replace(regex, '$1').split('/');
-  const list = ol({ class: 'breadcrumb' });
+  const list = ol({ class: 'breadcrumb', itemtype: 'https://schema.org/BreadcrumbList', itemscope: '' });
   const { origin } = window.location;
   let pagePath = '';
+  let metaContent = 1;
   pathSegments.forEach((page) => {
     pagePath += page;
-    const crumb = li({ class: 'crumb' });
+    const crumb = li({
+      class: 'crumb', itemprop: 'itemListElement', itemscope: '', itemtype: 'https://schema.org/ListItem',
+    });
     // This condition is for homepage path
     if (!pagePath) {
       const href = `${origin}${LANGUAGE_ROOT}/home`;
       const homeSvg = `${window.hlx.codeBasePath}/icons/home.svg`;
       // TODO:localize the title and alt
-      const homeLink = `<a href='${href}' title='Home'><img src='${homeSvg}' alt='Home'>
-                                      </img></a>`;
+      const homeLink = `<a itemprop='item' href='${href}' title='Home'><img itemprop='image'
+        src='${homeSvg}' alt='Home'></img></a><meta itemprop='position' content='${metaContent}'/>`;
       crumb.innerHTML = homeLink;
       const pipelineSymbol = span({ class: 'breadcrumb-separator' });
       crumb.append(pipelineSymbol);
@@ -54,15 +30,23 @@ export default async function decorate(block) {
     } else {
       const pageObj = results.filter((item) => item.path === pagePath);
       if (pageObj && pageObj.length === 1) {
+        metaContent += 1;
         const pageInfo = pageObj[0];
         const label = pageInfo.navTitle ? pageInfo.navTitle : pageInfo.title;
         if (pageInfo.path !== window.location.pathname) {
+          const labelElement = span({ itemprop: 'name' }, label);
           const anchor = a({
             class: 'breadcrumb',
             href: origin + pagePath,
             title: label,
-          }, label);
+            itemprop: 'item',
+          }, labelElement);
           crumb.append(anchor);
+          const meta = document.createElement('meta');
+          // Using setAttribute because meta.itemprop is not working to set itemprop attribute.
+          meta.setAttribute('itemprop', 'position');
+          meta.setAttribute('content', metaContent);
+          crumb.append(meta);
           const pipelineSymbol = span({
             class: 'breadcrumb-separator',
           });
@@ -73,10 +57,5 @@ export default async function decorate(block) {
     }
     pagePath = `${pagePath}/`;
   });
-  const currentPageLabel = getCurrentPageLabel(results);
-  const navTitleEl = document.createTextNode(currentPageLabel);
-  const crumb = li({ class: 'crumb' });
-  crumb.appendChild(navTitleEl);
-  list.append(crumb);
   block.innerHTML = list.outerHTML;
 }
