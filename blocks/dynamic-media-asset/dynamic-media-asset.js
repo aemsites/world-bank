1c03049c193daa80f0a9dcc1fbc7b577bbf3ce53
Rob Rusher <rrusher@adobe.com>
2024-10-01 12:04:45 -0600

    remove bug

--- blocks/dynamic-media-asset/dynamic-media-asset.js
+++ blocks/dynamic-media-asset/dynamic-media-asset.js
@@ -2,7 +2,7 @@ import { img } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
   const href = block.querySelector('a').getAttribute('href');
-  const altText = block.querySelector('p[data-aue-prop="dm_alttext"]').innerText;
+  const altText = block.querySelector('p[data-aue-prop="dm_alttext"]')?.innerText || '';
   block.innerHTML = '';
   block.append(img({ src: href, alt: altText }));
 }
