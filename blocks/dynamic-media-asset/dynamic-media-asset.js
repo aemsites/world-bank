288a816c0371ecd9d06d3590545ddc9eb04cc9cc
Rob Rusher <rrusher@adobe.com>
2024-10-01 11:54:38 -0600

    try optimized image

--- blocks/dynamic-media-asset/dynamic-media-asset.js
+++ blocks/dynamic-media-asset/dynamic-media-asset.js
@@ -1,8 +1,6 @@
-import { img } from '../../scripts/dom-helpers.js';
-
 export default async function decorate(block) {
   const href = block.querySelector('a').getAttribute('href');
   const altText = block.querySelector('p[data-aue-prop="dm_alttext"]').textContent;
   block.innerHTML = '';
-  block.append(img({ src: href, alt: altText }));
+  block.append(createOptimizedPicture(href, altText, 'lazy', [{ width: '800' }]));
 }
