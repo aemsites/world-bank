9024c2ad366ca8fd6cbc76c419f7ca49555f3674
Rob Rusher <rrusher@adobe.com>
2024-10-01 11:56:02 -0600

    try optimized image

--- blocks/dynamic-media-asset/dynamic-media-asset.js
+++ blocks/dynamic-media-asset/dynamic-media-asset.js
@@ -1,3 +1,5 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
 export default async function decorate(block) {
   const href = block.querySelector('a').getAttribute('href');
   const altText = block.querySelector('p[data-aue-prop="dm_alttext"]').textContent;
