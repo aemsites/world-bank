af04f08d53e8151de328eeaef86023d1699acb99
singhpritam18 <pritams@adobe.com>
2024-10-04 19:03:56 +0530

    Updated to fix the accessibility issue

--- blocks/signup/signup.js
+++ blocks/signup/signup.js
@@ -127,9 +127,13 @@ function attachFormValidation(block, placeholders) {
     errorMessage.textContent = ''; // Reset error message
     if (!validateEmail(emailInput.value)) {
       emailInput.classList.add('input-error');
+      emailInput.setAttribute('aria-invalid', 'true');
+      emailInput.setAttribute('aria-describedby', 'email-error');
       errorMessage.textContent = placeholders[CONSTANTS.SIGNUP_EMAIL_VALIDATION_MESSAGE] || 'Please enter a valid email.';
     } else {
       emailInput.classList.remove('input-error');
+      emailInput.setAttribute('aria-invalid', 'false');
+      emailInput.removeAttribute('aria-describedby');
     }
   });
 
@@ -152,6 +156,8 @@ function attachFormValidation(block, placeholders) {
     if (!validateEmail(email)) {
       errorMessage.textContent = placeholders[CONSTANTS.SIGNUP_EMAIL_VALIDATION_MESSAGE] || 'Please enter a valid email.';
       emailInput.classList.add('input-error');
+      emailInput.setAttribute('aria-invalid', 'true');
+      emailInput.setAttribute('aria-describedby', 'email-error');
       return;
     }
 
@@ -159,6 +165,8 @@ function attachFormValidation(block, placeholders) {
       termErrorMessage.textContent = placeholders[CONSTANTS.SIGNUP_TERMS_VALIDATION]
         || 'Please agree with the terms.';
       agreeInput.classList.add('input-error');
+      agreeInput.setAttribute('aria-invalid', 'true');
+      agreeInput.setAttribute('aria-describedby', 'terms-error');
       return;
     }
 
@@ -204,6 +212,7 @@ function createSignupModule(block, placeholders) {
           type: 'email',
           id: 'email',
           placeholder: ' ',
+          require: true,
         }),
         label({
           for: 'email',
@@ -224,18 +233,19 @@ function createSignupModule(block, placeholders) {
       ),
       button({ type: 'submit', id: 'signup-btn-desktop' }, span({ class: 'icon' }), placeholders[CONSTANTS.SIGNUP_BUTTON_TEXT] || 'Sign up'),
     ),
-    div({ class: 'error-message', id: 'error-message' }),
+    div({ class: 'error-message', id: 'error-message', role: 'alert' }),
     div(
       { class: 'input-group checkbox-group' },
       input({
         type: 'checkbox',
         id: 'agree',
+        require: true,
       }),
       label({
         for: 'agree',
       }, placeholders[CONSTANTS.SIGNUP_TERMS] || 'I agree with the terms of the Privacy Notice and consent to my personal data being processed, to the extent necessary, to subscribe to the selected updates.'),
     ),
-    div({ class: 'error-message', id: 'term-error-message' }),
+    div({ class: 'error-message', id: 'term-error-message', role: 'alert' }),
     button({ type: 'submit', id: 'signup-btn' }, span({ class: 'icon' }), placeholders[CONSTANTS.SIGNUP_BUTTON_TEXT] || 'Sign up'),
   );
 
